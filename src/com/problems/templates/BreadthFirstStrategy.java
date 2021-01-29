37d46e8daf9d5c651ac3dea2d70b7ff019db9a48
krishna <krishnapainuly@gmail.com>
2021-01-28 21:23:47 -0600

    BFS & DFS strategy

---
+++ src/com/problems/templates/BreadthFirstStrategy.java
@@ -0,0 +1,19 @@
+package com.problems.templates;
+
+public class BreadthFirstStrategy {
+
+	// Initialize and empty result array
+
+	// Handle an empty tree as a special edge case(return empty result array)
+
+	// Create an empty queue and push the root of the tree into it
+
+	// while queue is not empty
+		// count number of nodes in the queue
+		// Repeat that many times
+		// Pop the next node from the front of the Queue
+		// Append the value of the result array
+	
+		// If it has a left child, add the left child to the queue
+		// If it has a right child, add the right child to the queue
+}
---
+++ src/com/problems/templates/DepthFirstSearchStrategy.java
@@ -0,0 +1,18 @@
+package com.problems.templates;
+
+public class DepthFirstSearchStrategy {
+
+	// Handle an empty tree as a special edge case
+
+	// create a recursive function to execute on every node
+
+	// Function DFS(node)
+		// Base case: if leaf node, then do something if necessary
+		// Recursive case: Not a leaf Node
+		
+		// Pre-Order: Process info before visiting either child
+		// If the node has a LEFT child: dfs(node.left)
+		// In-Order: Process the information in sequence
+		// If the node has a RIGHT child: dfs(node.right)
+		// Post-Order: Process the information after both children have been visited
+}
